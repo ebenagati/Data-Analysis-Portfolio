7b577065efb885a1b84ac3f0e8fc17ba40aef719
ebenagati <ebenagati@gmail.com>
2025-02-22 02:01:36 +0000

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -149,7 +149,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'Dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
